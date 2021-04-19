e16c22015c23a894d4d3932ff088d280bd396b30
Dilip SIngh <dilipsingh.d98@gmail.com>
2021-04-19 23:15:14 +0100

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "root", "pmsdb");
+$con=mysqli_connect("api-db.cqapzzocktly.us-east-1.rds.amazonaws.com", "admin", "Dilip999", "pmsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
